7eba818128e410c60490e58543879ec48bb8ced3
Dwayne Boothe <dwboothe@gmail.com>
2013-07-09 06:35:35 +0000

    Updated to read from file

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var readFromFile = function(){
+    var fs = require('fs');
+    var inputFile = "index.html";
+    var buffer = fs.readFile(inputFile);
+    return buffer.toString();
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(readFromFile());
 });
 
 var port = process.env.PORT || 5000;
